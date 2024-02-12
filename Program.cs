377bb8e0fd2dc8370c25160997ecbf55b89e7841
heurilinares <heurilinares@DESKTOP-7F9G3G4>
2024-02-11 18:57:52 -1200

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("PAR O IMPAR");
+
+Console.WriteLine();
+
+{
+    
+    Console.WriteLine("Ingrese un número:");
+    int numero = Convert.ToInt32(Console.ReadLine());
+
+   
+    if (numero % 2 == 0)
+    {
+        Console.WriteLine("El número ingresado es par.");
+    }
+    else
+    {
+        Console.WriteLine("El número ingresado es impar.");
+    }
+}
